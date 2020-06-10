01f750fc8b01f488aabe0e5d28170e4903243b0f
bagasys <37441075+bagasys@users.noreply.github.com>
2020-06-10 13:48:18 +0700

    get Contacts for a specific user

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 // @route     GET api/contacts
 // @desc      Get all user's contacts
 // @access    Private
 
-router.get('/', (req, res) => {
-  res.send('Get all user\'s contacts');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({user: req.user.id}).sort({ date: -1 });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/contacts
