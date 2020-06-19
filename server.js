883ca28ffa92433525702da957803896c7f69975
bagasys <37441075+bagasys@users.noreply.github.com>
2020-06-19 14:51:49 +0700

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path')
 
 //Connect Database
 connectDB();
@@ -8,13 +9,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => { res.json({ msg: 'Welcome to the ContactKeeper API' })});
+// app.get('/', (req, res) => { res.json({ msg: 'Welcome to the ContactKeeper API' })});
 
 // Defines routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
