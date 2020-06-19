d79ef0dfc6231a86f6970e60049761c165f2e03d
bagasys <37441075+bagasys@users.noreply.github.com>
2020-06-19 14:30:30 +0700

    API Integration: update contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -70,8 +70,21 @@ const ContactState = props => {
   }
 
   //Update Contact 
-  const updateContact = contact => {
-    dispatch({type: UPDATE_CONTACT, payload: contact});
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+      dispatch({type: UPDATE_CONTACT, payload: res.data});
+    } catch (error) {
+      dispatch({type: CONTACT_ERROR, payload: error.response.msg});
+    }
+
+    
   }
   //Filter Contacts
   const filterContacts = text => {
